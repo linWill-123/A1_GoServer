23d8003da41e0ab5ce547c4e4aba58c033ec67fb
WillLIn1341425 <66269994+WillLIn1341425@users.noreply.github.com>
2023-10-09 14:19:49 -0700

    Change code get method to return pre-existing data

--- main.go
+++ main.go
@@ -25,7 +25,17 @@ type album struct {
 }
 
 // albums slice to seed record album data.
-var albums = []album{}
+var albums = []album{
+	{
+        ID: "1",
+        Image: nil,  
+        Profile: profile{
+            Artist: "artistName",
+            Title:  "titleName",
+            Year:   "yearValue",
+        },
+    },
+}
 
 // postAlbums adds an album from JSON received in the request body.
 func postAlbums(c *gin.Context) {
@@ -44,12 +54,12 @@ func postAlbums(c *gin.Context) {
 	}
 
 	var newAlbum album
-x
+
     newAlbum.ID = uuid.New().String()
 	newAlbum.Profile = newProfile
 	newAlbum.Image = image
 
-    albums = append(albums, newAlbum)
+    // albums = append(albums, newAlbum) // not going to add album to prevent memory overload
     c.JSON(http.StatusCreated, gin.H{"albumID": newAlbum.ID,"imageSize": image.Size})
 }
 
@@ -57,9 +67,10 @@ x
 // parameter sent by the client, then returns that album as a response.
 func getAlbumByID(c *gin.Context) {
     id := c.Param("id")
+	id = "1"
 
     // Loop over the list of albums, looking for
-    // an album whose ID value matches the parameter.
+    // an album whose ID value matches 1, which is id of our predefined data
     for _, a := range albums {
         if a.ID == id {
 			c.JSON(http.StatusCreated, gin.H{"artist": a.Profile.Artist, "title": a.Profile.Title, "year": a.Profile.Year})
